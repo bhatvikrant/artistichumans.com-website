456432beea3ba8f031a411a10cc2a485092c16de
Vikrant Bhat <vikrantbhat@Vikrants-MacBook-Pro.local>
2019-08-21 23:30:59 +0530

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Created by : Vikrant Bhat
+
+Check out my youtube channel: https://www.youtube.com/watch?v=3cgWwT4bpKU&list=PLPXR5c0kwv3FX8urLzWiSpYm9cqSSOVsN
+
+Follow me on instagram: https://www.instagram.com/code_with_vic/
---
+++ assets/js/my_edit.js
@@ -0,0 +1,34 @@
+// Created by : Vikrant Bhat
+
+// Check out my youtube channel: https://www.youtube.com/watch?v=3cgWwT4bpKU&list=PLPXR5c0kwv3FX8urLzWiSpYm9cqSSOVsN
+
+// Follow me on instagram: https://www.instagram.com/code_with_vic/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
